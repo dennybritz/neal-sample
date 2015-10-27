2eb4b3133c3266a595b534fdd3c104af2b7837fa
Denny Britz <dennybritz@gmail.com>
2015-10-27 11:32:17 +0100

    Landing page changes

--- js/sample-page.jsx
+++ js/sample-page.jsx
@@ -40,7 +40,7 @@ let businessAddress = (
 );
 
 let pricingPlan1 = {
-  name: "Starter",
+  name: "Personal",
   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
   price: "$99",
   features: {
@@ -122,7 +122,16 @@ export default (props) => {
         </p>
       </Hero>
 
-      <Section heading="What it looks like...">
+      <Section className="subhero">
+        <ImageList centered>
+          <ImageListItem src="img/press/cnn-logo.png" url="http://www.cnn.com"/>
+          <ImageListItem src="img/press/forbes-logo.png" url="http://forbes.com/"/>
+          <ImageListItem src="img/press/theverge-logo.png" url="http://www.theverge.com/"/>
+          <ImageListItem src="img/press/techcrunch-logo.jpg" url="http://techcrunch.com/"/>
+        </ImageList>
+      </Section>      
+
+      <Section className="nopad-bottom">
         <Code lang="jsx" block>{sampleCode}</Code>
       </Section>
 
@@ -130,16 +139,20 @@ export default (props) => {
         <HorizontalSplit padding="md">
           <div>
             <p className="lead">Batteries Included</p>
-            <p>Neal ships with a navbar, hero unit, footer, sections, horziontal split (this text), and all the other basic elements you need for a landing page. No more repetetive coding!</p>
+            <p>Neal ships with a navbar, hero unit, footer, sections, horziontal split, pricing tables, customer quotes other other basic components you need for a landing page. No more repetetive coding! Oh, and it's easy to extend.</p>
           </div>
           <div>
             <p className="lead">Third-Party Integrations</p>
-            <p>Neal includes integration components for <a>Google Analytics</a>, <a>Segment</a>, and <a>Stripe</a>. No more copying & pasting integration code, all you need is your API keys. We automatically track events when visitors navigate to different parts of your page, using the react-router library.</p>
+            <p>Neal includes integration components for&nbsp;
+              <a href="http://www.google.com/analytics/">Google Analytics</a>,&nbsp;
+              <a href="https://segment.com/">Segment</a>,&nbsp;
+              <a href="https://stripe.com/">Stripe</a> and&nbsp;
+              <a href="http://typeform.com">Typeform</a>.
+              No more copying & pasting integration code, all you need is your API keys. We automatically track events when visitors navigate to different parts of your page.</p>
           </div>
           <div>
-            <p className="lead">Open-source. Used in production. </p>
-            <p>I use Neal in production, so you can be sure it's maintained.
-            </p>
+            <p className="lead">Serverless Deployment</p>
+            <p>Because you are relying on react.js and third-party integration you don't need a server to host your landing page. Simply upload it to an Amazon S3 bucket, enable website hosting, and it's ready to go!</p>
           </div>
         </HorizontalSplit>
       </Section>
@@ -153,7 +166,7 @@ export default (props) => {
         </p>
       </Section>
 
-      <Section heading="Pricing Table">
+      <Section>
         <PricingTable>
           <PricingPlan {... pricingPlan1} />
           <PricingPlan {... pricingPlan2} />
@@ -161,21 +174,21 @@ export default (props) => {
         </PricingTable>
       </Section>
 
-      <Section heading="Customer Quotes">
+      <Section>
         <CustomerQuotes>
-          <CustomerQuote name="Denny" title="Co-founder" imageUrl="img/people/paulgraham.jpg">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <CustomerQuote name="Paul Graham" title="YC" imageUrl="img/people/paulgraham.jpg">
+            <p>What I tell founders is not to sweat the business model too much at first. The most important task at first is to build something people want. If you don't do that, it won't matter how clever your business model is.</p>
           </CustomerQuote>
-          <CustomerQuote name="Denny" title="Co-founder" imageUrl="img/people/elonmusk.jpg">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <CustomerQuote name="Elon Musk" imageUrl="img/people/elonmusk.jpg">
+            <p>I came to the conclusion that we should aspire to increase the scope and scale of human consciousness in order to better understand what questions to ask. Really, the only thing that makes sense is to strive for greater collective enlightenment.</p>
           </CustomerQuote>
-          <CustomerQuote name="Denny" title="Co-founder" imageUrl="img/people/reidhoffman.jpg">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <CustomerQuote name="Reid Hoffman" title="Linkedin" imageUrl="img/people/reidhoffman.jpg">
+            <p>If you are not embarrassed by the first version of your product, you've launched too late.</p>
           </CustomerQuote>
         </CustomerQuotes>
       </Section>
 
-      <Section heading="Team">
+      <Section>
         <Team>
           <TeamMember name="Denny Britz" title="Co-founder" imageUrl="img/people/paulgraham.jpg">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
@@ -187,16 +200,6 @@ export default (props) => {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
           </TeamMember>
         </Team>
-      </Section>
-
-      <Section heading="As seeen on...">
-        <ImageList centered={false}>
-          <ImageListItem src="img/press/cnn-logo.png" url="http://www.cnn.com"/>
-          <ImageListItem src="img/press/forbes-logo.png" url="http://forbes.com/"/>
-          <ImageListItem src="img/press/theverge-logo.png" url="http://www.theverge.com/"/>
-          <ImageListItem src="img/press/techcrunch-logo.jpg" url="http://techcrunch.com/"/>
-          <ImageListItem src="img/press/venturebeat-logo.jpg" url="http://venturebeat.com/"/>
-        </ImageList>
       </Section>
 
       <Footer brandName={brandName} facebookUrl="http://www.facebook.com" twitterUrl="http://www.twitter.com"
